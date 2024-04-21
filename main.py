596f8fd04571579f1bfafef759dbf764acea65fa
Deepak Agarwal <deep.agarwal@hotmail.com>
2024-04-21 16:59:18 +0530

    Added FAISS vector store implementation

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 import os
 
 from langchain.chains.retrieval_qa.base import RetrievalQA
-from langchain_community.document_loaders import TextLoader
-from dotenv import load_dotenv
-from langchain_text_splitters import CharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
 from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
-from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
 
 load_dotenv()
 
 if __name__ == '__main__':
-    text_loader = TextLoader(f"{os.getcwd()}/text_loader/medium_blog_post.txt", encoding='utf8')
-    document = text_loader.load()
+    pdf_path = f"{os.getcwd()}/pdf_loader/2210.03629.pdf"
+    loader = PyPDFLoader(file_path=pdf_path)
+    document = loader.load()
 
-    text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_documents(document)
+    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=3)
+    docs = text_splitter.split_documents(document)
+
     embeddings = AzureOpenAIEmbeddings(
         azure_deployment="embeddings",
         openai_api_version=os.getenv("OPENAI_API_EMBEDDINGS_VERSION"),
     )
-    document_search = PineconeVectorStore.from_documents(texts, embeddings, index_name="medium-blog-post-index")
+
+    document_search = FAISS.from_documents(docs, embeddings)
+
+    # You can optionally save the vector store index locally on the hard-disk and load from it.
+    # Else by default, it gets saved in RAM
+    # document_search.save_local("faiss_index_react")
+    # document_search = FAISS.load_local("faiss_index_react", embeddings, allow_dangerous_deserialization=True)
 
     llm = AzureChatOpenAI(azure_deployment='turbo', temperature=0.1)
     retrieval_qa = RetrievalQA.from_chain_type(
         llm=llm,
-        chain_type="stuff",
+        chain_type='stuff',
         retriever=document_search.as_retriever(),
         return_source_documents=True)
-    query = "What is a vector database? Give me a 15 words answer for a beginner"
+
+    query = "Give me a gist of ReAct in 3 sentences"
     result = retrieval_qa({"query": query})
     print(result)
-
-
-
-
